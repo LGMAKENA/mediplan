dad6db5f7a069dd12c6a9a543e6064421553896a
lgmakena <linetmakenag@gmail.com>
2023-09-04 15:52:04 +0300

    controllers

---
+++ app/controllers/doctors_controller.rb
@@ -0,0 +1,7 @@
+class DoctorsController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+end
---
+++ app/controllers/patients_controller.rb
@@ -0,0 +1,7 @@
+class PatientsController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+end
