3d7a832d0149712428cbf6c3555398aaa7e62798
Alex <alexkimanthi92@gmail.com>
2023-09-04 17:16:06 +0300

    updated patients controller

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,7 +1,20 @@
 class PatientsController < ApplicationController
   def index
+    patients = Patient.all 
+    render json: patients 
   end
 
-  def create
+  def create 
+    patient = Patient.new(patient_params)
+    if patient.save 
+      render json: patient 
+    else
+      render json: { error: "Not created"}, status: :unauthorized 
+    end
+  end
+
+  private
+  def patient_params 
+    params.permit(:email, :username, :dob, :gender) 
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   get 'doctors',to: "doctors#index"
   get 'appointments',to: 'appointments#index'
   post 'appointments',to: 'appointments#create'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :patients 
+
+  post "patients", to: "patients#create"
 end
