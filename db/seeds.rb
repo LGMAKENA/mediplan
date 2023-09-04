f39d2caa8f1262b8f18a04647b00b3d689b4bca2
lgmakena <linetmakenag@gmail.com>
2023-09-04 12:57:13 +0300

    adding seeding data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Destroying existing data..."
+Doctor.destroy_all 
+Patient.destroy_all
+puts "🌱 Seeding spices..." 
+Doctor.create(title: "Dentist",email: "linet@test.com",full_name: "Linet Makena", username: "lgmakena",password: "Pass@123",dob: "1900-12-12",gender: "male")
+
+
+patient1 = Patient.create( username: "lgmakena", email:"kmusau@test.com",password: "Pass@123",dob: "1990-7-12",gender: "male")
+patient2 = Patient.create( username: "akimathi", email:"akimathi@test.com",password: "Pass@123",dob: "1970-12-12",gender: "male")
+patient3 = Patient.create( username: "akulola", email:"akulola@test.com",password: "Pass@123",dob: "1950-2-12",gender: "female")
+
+
+
+
+
+
+
+puts "✅ Done seeding!"
