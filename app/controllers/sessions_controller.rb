ced1159861c757119bdca2b3ebb00f7c69803eb8
Kennedy Musau <kennedy.musauu@gmail.com>
2023-09-06 17:44:08 +0300

    fix session controller from User to Patient

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,29 +1,27 @@
 class SessionsController < ApplicationController
-
-    def create 
-        patient = User.find_by(username: params[:username])
-        if patient&.authenticate(params[:password])
-            session[:patient_id] = patient.id 
-            render json: patient     
-        
-        else
-            errors = ["Invalid username or password"]
-            # if user.nil?
-            #     errors << 'invalid password'
-            # else 
-            #     errors << 'invalid username'
-            # end
-            render json: { errors: errors }, status: :unauthorized 
-        end 
+  def create
+    patient = Patient.find_by(username: params[:username])
+    if patient&.authenticate(params[:password])
+      session[:patient_id] = patient.id
+      render json: patient
+    else
+      errors = ["Invalid username or password"]
+      # if user.nil?
+      #     errors << 'invalid password'
+      # else
+      #     errors << 'invalid username'
+      # end
+      render json: { errors: errors }, status: :unauthorized
     end
+  end
 
-    def destroy      
-       if logged_in?
-        session.delete :patient_id
-        head :no_content 
-       else
-        errors = ['Not logged in']
-        render json: { errors: errors }, status: :unauthorized
-       end
-    end 
-end 
+  def destroy
+    if logged_in?
+      session.delete :patient_id
+      head :no_content
+    else
+      errors = ["Not logged in"]
+      render json: { errors: errors }, status: :unauthorized
+    end
+  end
+end
