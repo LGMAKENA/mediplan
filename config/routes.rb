c527576f6dfff8fb5b6e33a85c8249b9e87ef3c3
Kennedy Musau <kennedy.musauu@gmail.com>
2023-09-06 17:51:29 +0300

    update session routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,20 @@
 Rails.application.routes.draw do
-
-  resources :appointments, only: [:show, :index, :create, :update, :destroy ] 
+  resources :appointments, only: [:show, :index, :create, :update, :destroy]
   resources :patients
-  resources :doctors  
+  resources :doctors
 
   # get 'doctors',to: "doctors#index"
   # # get 'appointments',to: 'appointments#index'
   # post 'appointments',to: 'appointments#create'
 
-  post "/signup", to: "patients#create" 
-  get "/me", to: "doctors#show"
-  get "/me", to: "patients#show"
-  post "login", to:"sessions#create" 
-  delete "/logout", to: "sessions#destroy" 
-  post "loginDoc", to:"doctor_sessions#create"
-  delete "/logoutDoc", to: "doctor_sessions#destroy" 
-
-
+  post "/signup", to: "patients#create"
+  get "/medoc", to: "doctors#show"
+  get "/mepatient", to: "patients#show"
+  post "login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  post "loginDoc", to: "doctor_sessions#create"
+  delete "/logoutDoc", to: "doctor_sessions#destroy"
 
   # post "patients", to: "patients#create"
-  # post "doctors", to: "doctors#create" 
+  # post "doctors", to: "doctors#create"
 end
