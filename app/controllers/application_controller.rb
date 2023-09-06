81993d417bb03ece6cd78aaea11f58e185e0cc7c
Kennedy Musau <kennedy.musauu@gmail.com>
2023-09-07 00:49:29 +0300

    update appointment controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,15 +24,20 @@ class ApplicationController < ActionController::API
   include ActionController::Cookies
 
   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   # before_action :authorize
 
   def authorize
     # @current_user = Patient.find_by(id: session[:patient_id])
-    render json: { errors: ["Not Authorized"] }, status: :unauthorized unless session.include? patient_id
+    render json: { errors: ["Not Authorized"] }, status: :unauthorized unless session.include? :patient_id
   end
 
   def render_unprocessable_entity_response(invalid)
     render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+  end
+
+  def render_not_found_response
+    render json: { error: "Bird not found" }, status: :not_found
   end
 end
