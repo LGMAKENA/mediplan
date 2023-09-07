17c55fe885bcba5cf046f9c07d151417c27cef24
Kennedy Musau <126899757+Ken-Musau@users.noreply.github.com>
2023-09-07 08:16:06 +0300

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,14 @@
 class ApplicationController < ActionController::API
-    include ActionController::Cookies
+  include ActionController::Cookies
 
-    private
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
-    def current_user
-        @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
-    end
-  
-    def require_login
-      unless logged_in?
-      errors = ['Not logged in']
-      render json: { errors: errors }, status: :unauthorized
-      end
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
   end
-  
-  def logged_in?
-  #    !session[:user_id].nil?
-     session.include? :patient_id
+
+  def render_not_found_response
+    render json: { error: "Bird not found" }, status: :not_found
   end
 end
