1eddc568a55a80b7dd10cfd33f6a7dcd107b86aa
Kennedy Musau <126899757+Ken-Musau@users.noreply.github.com>
2023-09-07 08:18:06 +0300

    Update doctors_controller.rb

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,23 +1,22 @@
 class DoctorsController < ApplicationController
-  # before_action :require_login 
+  before_action :authorize_doctor
 
   def index
-    doctors = Doctor.all 
-    render json: doctors 
+    render json: Doctor.all
   end
 
-  def show 
-    doctor = Doctor.find_by(id: session[:doctor_id]) 
-    if doctor
-        render json: doctor 
-    else
-        render json: { error: "not authorized here" }, status: :unauthorized 
-    end 
+  def show
+    doctor = Doctor.find_by(id: session[:doctor_id])
+    render json: doctor
   end
-
 
   private
-  def doctor_params 
-    params.permit(:title, :full_name, :email, :username, :dob, :gender) 
+
+  def doctor_params
+    params.permit(:title, :full_name, :email, :username, :dob, :gender)
+  end
+
+  def authorize_doctor
+    return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :doctor_id
   end
 end
