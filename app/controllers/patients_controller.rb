d4ca94611beeea25e7ee561a1076da9e36d2d6b7
Kennedy Musau <kennedy.musauu@gmail.com>
2023-09-07 17:30:44 +0300

    update authorize

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,6 +1,6 @@
 class PatientsController < ApplicationController
   before_action :authorize_patient
-  skip_before_action :authorize_patient, only: :create
+  skip_before_action :authorize_patient, only: [:create, :index]
 
   def index
     render json: Patient.all
