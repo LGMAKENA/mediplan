bd39943e59a098ad7cac123680be528e5fda02f0
Kennedy Musau <126899757+Ken-Musau@users.noreply.github.com>
2023-09-07 08:17:44 +0300

    Update doctor_sessions_controller.rb

--- app/controllers/doctor_sessions_controller.rb
+++ app/controllers/doctor_sessions_controller.rb
@@ -1,29 +1,16 @@
 class DoctorSessionsController < ApplicationController
-
-    def create 
-        doctor = Doctor.find_by(username: params[:username])
-        if doctor&.authenticate(params[:password])
-            session[:doctor_id] = doctor.id 
-            render json: doctor    
-        
-        else
-            errors = ["Invalid username or password"]
-            # if user.nil?
-            #     errors << 'invalid password'
-            # else 
-            #     errors << 'invalid username'
-            # end
-            render json: { errors: errors }, status: :unauthorized 
-        end 
+  def create
+    doctor = Doctor.find_by(username: params[:username])
+    if doctor&.authenticate(params[:password])
+      session[:doctor_id] = doctor.id
+      render json: doctor, status: :created
+    else
+      render json: { error: ["Invalid username or password"] }, status: :unauthorized
     end
+  end
 
-    def destroy      
-       if logged_in?
-        session.delete :doctor_id
-        head :no_content 
-       else
-        errors = ['Not logged in']
-        render json: { errors: errors }, status: :unauthorized
-       end
-    end 
+  def destroy
+    session.delete :doctor_id
+    head :no_content
+  end
 end
